27f4d12d75492d28fcf3503a9d5e69d5bcc074be
aitorgaraza <aitorgaflo@gmail.com>
2020-05-21 18:58:03 +0200

    21/5

---
+++ UltimateDarkNova/Assets/Script/Character_controller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_controller : MonoBehaviour
+{
+
+    public float force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxisRaw("Horizontal");
+        gameObject.GetComponent<Rigidbody2D>().velocity= new Vector2(move * Time.deltaTime * force, 0.0f);
+    }
+}
---
+++ UltimateDarkNova/Assets/Script/Generador.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generador : MonoBehaviour
+{
+    public GameObject navelvl1;
+
+    float sactuales;
+    public float seconds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sactuales = seconds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sactuales -= Time.deltaTime;
+
+        if (sactuales <= 0.0f)
+        {
+            Instantiate(navelvl1, transform.position = new Vector2(0.0f, 8.0f), transform.rotation = Quaternion.Euler(180, 0, 0));
+
+            sactuales = seconds;
+        }
+    }
+}
