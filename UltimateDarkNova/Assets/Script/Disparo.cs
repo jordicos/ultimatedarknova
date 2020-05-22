99a5c2afe6cf5f1384b909c8112a922d81b56cb9
jordicos <jordi.cos.gabarron@gmail.com>
2020-05-22 19:36:00 +0200

    22/05/20

--- UltimateDarkNova/Assets/Script/Disparo.cs
+++ UltimateDarkNova/Assets/Script/Disparo.cs
@@ -9,22 +9,34 @@ public class Disparo : MonoBehaviour
     private Rigidbody2D rb;
     public GameObject explosion;
 
+    float sactuales;
+    public float seconds;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(0, speed);
+
+        sactuales = seconds;
     }
 
     // Update is called once per frame
     void Update()
     {
+        sactuales -= Time.deltaTime;
+
+        if(sactuales <= 0.0f)
+        {
+            sactuales = seconds;
+        }
+
         if (transform.position.y > 5.37)
         {
             Destroy(this.gameObject);
         }
     }
-    private void OnTriggerEnter2D(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "navelvl1")
         {
@@ -32,6 +44,8 @@ public class Disparo : MonoBehaviour
             e.transform.position = transform.position;
             Destroy(other.gameObject);
             Destroy(this.gameObject);
+            Destroy(e.gameObject, 0.15f);
+            
         }
     }
 }
